403d61e1ca18805166730ca6ef68b2316003582f
MaxiViP <batti@ya.ru>
2025-09-17 10:59:03 +0300

    backend diploy vite_api_base frontend

--- backend/src/server/routes/index.ts
+++ backend/src/server/routes/index.ts
@@ -3,8 +3,6 @@ import { newsHandler } from '../handlers/news.js'
 
 export const apiRouter = Router()
 
-// ✅ теперь это будет доступно по /api/news
-// потому что в server/index.ts стоит app.use('/api', apiRouter)
 apiRouter.get('/news', newsHandler)
 
 // можно добавить healthcheck (удобно для проверки)
