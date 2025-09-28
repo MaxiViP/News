f552c8432684c6a99e9f6d2b166f8069bc68aac1
MaxiViP <batti@ya.ru>
2025-09-28 14:18:42 +0300

    сокращение

--- frontend/src/components/LiveMatches.vue
+++ frontend/src/components/LiveMatches.vue
@@ -11,7 +11,7 @@
 						: 'bg-gray-200 dark:bg-gray-700 hover:bg-gray-300 dark:hover:bg-gray-600 text-gray-800 dark:text-gray-200',
 				]"
 			>
-				📊 Горизонтально
+				📊Все
 			</button>
 			<button
 				@click="mode = 'marquee'"
@@ -47,7 +47,7 @@
 				to="/tables"
 				class="px-4 py-2 rounded-lg transition-all bg-gray-200 dark:bg-gray-700 hover:bg-gray-300 dark:hover:bg-gray-600 text-gray-800 dark:text-gray-200"
 			>
-				📊 Турнирные таблицы
+				📊 Таблицы
 			</RouterLink>
 		</div>
 
