467b2990db7b1d02774d5d0e8536d28a742fb8c0
MaxiViP <batti@ya.ru>
2025-09-28 15:18:54 +0300

    api token 10

--- backend/dist/server/routes.js
+++
@@ -1,5 +0,0 @@
-import { Router } from 'express';
-import { newsHandler } from './handlers/news.js';
-export const apiRouter = Router();
-// GET https://maxivip-news-ba8f.twc1.net/api/news?category=tech&country=ru&page=1
-apiRouter.get('/news', newsHandler);
